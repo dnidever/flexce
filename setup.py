1c7ca3c1994e2bf6f6c0302b69398afed0858d15
David Nidever <dnidever@montana.edu>
2025-10-13 14:54:05 -0600

    added setup.py and updated flexce.py

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
-from distutils.core import setup
+import os
+import sys
+import shutil
+from setuptools import setup, find_packages, find_namespace_packages
+from setuptools.command.install import install
+#from distutils.core import setup
 
-NAME = 'flexCE'
+#NAME = 'flexCE'
 # do not use x.x.x-dev.  things complain.  instead use x.x.xdev
-VERSION = '1.0.1dev'
-RELEASE = 'dev' not in VERSION
+#VERSION = '1.0.1dev'
+#RELEASE = 'dev' not in VERSION
 
-setup(name=NAME,
-      version=VERSION,
+setup(name='flexce',
+      version='1.0.1',
       description='Flexible Galactic Chemical Evolution Model',
       author='Brett Andrews',
       author_email='brett.h.andrews@gmail.com',
-      url='https://github.com/bretthandrews/flexCE',
-      license='MIT',
-      packages=['flexCE'])
+      url='https://github.com/dnidever/flexce',
+      requires=['numpy','astropy(>=4.0)','scipy','dlnpyutils','doppler'],
+      zip_safe = False,
+      include_package_data=True,
+      packages=find_namespace_packages(where="python"),
+      package_dir={"": "python"}
+)
